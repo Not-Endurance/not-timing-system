ad84a1154d1dc4545c6a2aa61f7f383f1f86775f
achobanov <alexchobanov@gmail.com>
2025-03-02 00:25:50 +0200

    Remove NTS.Nexus.Functions project

--- src/Nexus/NTS.Nexus.Functions/Models/Model.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NTS.Nexus.Functions.Models;
-
-public record Model
-{
-    public string TenantId { get; set; } = default!;
-    public int Id { get; set; }
-    public string Name { get; set; } = default!;
-}
