3953a28595b0709af9163f22024a30ffa35ccb12
achobanov <alexchobanov@gmail.com>
2025-03-08 20:07:31 +0200

    Add spinner in NButtonBase
    It's not ideal from pretty-stand point, but works atm
    
    https://github.com/Not-Endurance/not-timing-system/issues/356

---
+++ src/Not/Not.Blazor/Components/Mud/Buttons/Base/NButtonBase.razor.cs
@@ -0,0 +1,49 @@
+using MudBlazor;
+using Not.Localization;
+
+namespace Not.Blazor.Components;
+
+public abstract partial class NButtonBase : MudButton
+{
+    [Inject]
+    ILocalizer Localizer { get; set; } = default!;
+
+    [Parameter]
+    public string? Text { get; set; }
+
+    public bool IsLoading { get; protected set; }
+
+    protected override void OnParametersSet()
+    {
+        ChildContent =
+            Text == null ? null : new RenderFragment(x => x.AddContent(0, Localizer.Get(Text)));
+    }
+
+    protected async Task HandleClick()
+    {
+        try
+        {
+            IsLoading = true;
+            if (OnClick.HasDelegate)
+            {
+                await OnClick.InvokeAsync(null);
+            }
+        }
+        finally
+        {
+            IsLoading = false;
+        }
+    }
+
+    protected Color SelectColor()
+    {
+        if (Color == Color.Primary)
+        {
+            return Color.Secondary;
+        }
+        else
+        {
+            return Color.Primary;
+        }
+    }
+}
