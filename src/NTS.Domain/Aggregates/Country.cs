153f9d4989653855e5878a43c4a35877acced832
achobanov <alexchobanov@gmail.com>
2025-03-09 20:22:31 +0200

    Remove validation from country
    Countries are static anyway
    
    https://github.com/Not-Endurance/not-timing-system/issues/356

--- src/NTS.Domain/Aggregates/Country.cs
+++ src/NTS.Domain/Aggregates/Country.cs
@@ -4,16 +4,16 @@ namespace NTS.Domain.Aggregates;
 
 public class Country : AggregateRoot, IAggregateRoot
 {
-    public Country(int id, string? isoCode, string? nfCode, string name) : base(id)
+    public Country(int id, string? isoCode, string? nfCode, string? name) : base(id)
     {
-        Name = Required(nameof(Name), name);
-        IsoCode = Required(nameof(IsoCode), isoCode);
+        Name = name ?? "Default Name";
+        IsoCode = isoCode ?? "ISO";
         NfCode = nfCode;
     }
 
     public string IsoCode { get; }
-    public string? NfCode { get; }
     public string Name { get; }
+    public string? NfCode { get; }
 
     public override string ToString()
     {
