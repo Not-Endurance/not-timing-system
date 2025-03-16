8dbfb7b1f57d2bd53fa66a98a5aca8a8f51defe1
achobanov <alexchobanov@gmail.com>
2025-03-16 19:36:03 +0200

    Register Enum as string convention in BSON
    https://github.com/Not-Endurance/not-timing-system/issues/366

--- src/Nexus/NTS.Nexus.HTTP/Mongo/ServiceCollectionExtensions.cs
+++ src/Nexus/NTS.Nexus.HTTP/Mongo/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson;
 
 namespace NTS.Nexus.HTTP.Mongo;
 
@@ -6,6 +8,12 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddMongo(this IServiceCollection services, string? connectionString)
     {
+        var pack = new ConventionPack
+        {
+            new EnumRepresentationConvention(BsonType.String)
+        };
+        ConventionRegistry.Register("EnumStringConvention", pack, t => true);
+
         services.AddSingleton<IMongoContext, MongoContext>(x => new MongoContext(connectionString));
         return services;
     }
