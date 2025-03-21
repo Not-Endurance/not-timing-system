efc7dcfdc6399fa1880e04b40058fd53ad54a791
achobanov <alexchobanov@gmail.com>
2025-03-21 12:53:18 +0200

    Remove StaticOptions remnants

--- src/Not/Not.Storage/Stores/StoreServiceCollectionExtensions.cs
+++ src/Not/Not.Storage/Stores/StoreServiceCollectionExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Not.Domain.Ports;
 using Not.Filesystem;
 using Not.Storage.Stores.Files;
-using Not.Storage.Stores.StaticOptions;
 
 namespace Not.Storage.Stores;
 
@@ -22,23 +20,6 @@ public static class StoreServiceCollectionExtensions
         var factory = FileContextHelper.CreateFileContextFactory(configure, "stores");
         services.AddKeyedSingleton<IFileContext, FileContext>(StoreConstants.DATA_KEY, factory);
         services.AddSingleton(typeof(IStore<>), typeof(LockingJsonFileStore<>));
-        return services;
-    }
-
-    /// <summary>
-    /// FileContext defaults to <seealso cref="FileContextHelper.GetAppDirectory(string)"/> using 'Resources' as subdirectory
-    /// </summary>
-    /// <param name="services"></param>
-    /// <param name="configure">Custom configuration</param>
-    /// <returns></returns>
-    public static IServiceCollection AddStaticOptionsStore(
-        this IServiceCollection services,
-        Action<FileContext>? configure = null
-    )
-    {
-        var factory = FileContextHelper.CreateFileContextFactory(configure, "Resources");
-        services.AddKeyedSingleton<IFileContext, FileContext>(StoreConstants.STATIC_OPTIONS_STORE_KEY, factory);
-        services.AddSingleton(typeof(IStaticOptionsProvider<>), typeof(JsonStaticOptionsProvider<>));
         return services;
     }
 }
