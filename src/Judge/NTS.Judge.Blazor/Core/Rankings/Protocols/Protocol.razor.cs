c35947670fa39635a33c454069bbd3eed9779dcc
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2025-03-24 15:43:55 +0200

    add document header and official list to Ranking Protocol
    https://github.com/Not-Endurance/not-timing-system/issues/378

--- src/Judge/NTS.Judge.Blazor/Core/Rankings/Protocols/Protocol.razor.cs
+++ src/Judge/NTS.Judge.Blazor/Core/Rankings/Protocols/Protocol.razor.cs
@@ -1,9 +1,22 @@
 using NTS.Domain.Core.Objects;
+using NTS.Domain.Core.Objects.Documents;
 
 namespace NTS.Judge.Blazor.Core.Rankings.Protocols;
 
 public partial class Protocol
 {
+    string? _documentSubtitle;
+
     [Parameter, EditorRequired]
     public Ranklist Ranklist { get; set; } = default!;
+
+    [Parameter, EditorRequired]
+    public RanklistDocument Document { get; set; } = default!;
+
+    protected override void OnParametersSet()
+    {
+        _documentSubtitle = Document.Header.PopulatedPlace.City + "   " +
+        Document.Header.EventSpan + "   " +
+        Document.Header.PopulatedPlace.Country;
+    }
 }
