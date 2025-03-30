3f45a7274989e05102e8e22085c3f602b0b6db1d
achobanov <alexchobanov@gmail.com>
2025-03-30 16:14:57 +0300

    Localize Startlist table headers
    https://github.com/Not-Endurance/not-timing-system/issues/409

--- src/Judge/NTS.Judge.Blazor/Core/Startlists/StartlistTabs.cs
+++ src/Judge/NTS.Judge.Blazor/Core/Startlists/StartlistTabs.cs
@@ -5,20 +5,20 @@ namespace NTS.Judge.Blazor.Core.Startlists;
 
 public abstract class StartlistTabs : NComponent
 {
-    protected Dictionary<string, List<StartlistEntry>> StartlistsByStage { get; set; } = [];
+    protected Dictionary<string, List<StartlistEntry>> StartlistsByStage { get; } = [];
 
-    protected virtual string[] TableHeaders { get; } = ["Number", "Athlete", "Loop", "Start Time"];
+    protected virtual string[] TableHeaders { get; } = [Number_string, Athlete_string, Loops_string, Start_Time_string];
 
     protected void CreateStartlistsByStage(IEnumerable<StartlistEntry> starts)
     {
         foreach (var start in starts)
         {
             var tabHeader = $"{Stage_string} {start.PhaseNumber}";
-            if (!StartlistsByStage.Keys.Any(t => t == tabHeader))
+            if (StartlistsByStage.Keys.All(t => t != tabHeader))
             {
                 StartlistsByStage.Add(tabHeader, []);
             }
-            if (!StartlistsByStage[tabHeader].Any(s => s.Number == start.Number))
+            if (StartlistsByStage[tabHeader].All(s => s.Number != start.Number))
             {
                 StartlistsByStage[tabHeader].Add(start);
             }
