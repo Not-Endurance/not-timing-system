2fd6f3ef6411baabb7f06bbd5bd2a9886ab98925
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2025-04-19 22:17:14 +0300

    export component code in *.razor.cs format files
    https://github.com/Not-Endurance/not-timing-system/issues/436

---
+++ src/Witness/NTS.Witness/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,17 @@
+﻿
+namespace NTS.Witness.Components.Layout;
+
+public partial class MainLayout
+{
+    bool _drawerOpen = false;
+    
+    void ToggleDrawer()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+    
+    void CloseDrawer()
+    {
+        _drawerOpen = false;
+    }
+}
