816ae03b9625a7613783626ece7c703f65ddf4ee
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2025-04-24 20:37:25 +0300

    add WitnessPage components

--- src/Witness/NTS.Witness/Components/Pages/WitnessPage.razor.cs
+++ src/Witness/NTS.Witness/Components/Pages/WitnessPage.razor.cs
@@ -5,6 +5,10 @@ namespace NTS.Witness.Components.Pages;
 
 public partial class WitnessPage
 {
+    bool IsUserOfficial { get; set; } = false;
+
+    bool IsUserLoggedIn { get; set; } = false;
+
     [Inject]
     ITestBehind TestBehind { get; set; } = default!;
 
