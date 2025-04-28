ace85f5bee0dfbef8a1cb575d64d74964ad04508
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2025-04-29 00:20:09 +0300

    fix casting TimeSpan ToDateTimeOffset and result is in the past
    because TimeSpan value is earlier than the current time and
    ToDateTimeOffset uses DateTime.Today

--- src/Not/Not/Extensions/DateTimeOffsetExtension.cs
+++ src/Not/Not/Extensions/DateTimeOffsetExtension.cs
@@ -9,9 +9,20 @@ public static class DateTimeOffsetExtension
         return offsetTime;
     }
 
-    public static DateTimeOffset ToDateTimeOffset(this TimeSpan timeToBeAdded)
+
+    /// <summary>
+    /// ToDateTimeOffset creates a new DateTimeOffset from a TimeSpan. 
+    /// The DateTimeOffset's day defaults to the one following the current date to ensure scheduling events doesn't happen in the past.
+    /// To set the date to the current day pass 'true' as the second argument.
+    /// </summary>
+    public static DateTimeOffset ToDateTimeOffset(this TimeSpan timeToBeAdded, bool enforceDateToCurrentDay = false)
     {
-        var today = DateTime.Today;
+        var daysToAdd = 1;
+        if (enforceDateToCurrentDay)
+        {
+            daysToAdd = 0;
+        }
+        var today = DateTime.Today.AddDays(daysToAdd);
         var time = today.Add(timeToBeAdded);
         var timeWithSpecifiedKind = DateTime.SpecifyKind(time, DateTimeKind.Local);
         var offsetTime = timeWithSpecifiedKind;
