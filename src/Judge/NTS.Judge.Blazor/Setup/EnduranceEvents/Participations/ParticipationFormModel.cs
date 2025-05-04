74ac98dca6bf20b2057a8c17c018c82ce4bdcbec
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2025-05-04 14:49:28 +0300

    fix ParticipationFormModel and validate values when converting to Domain Participation

--- src/Judge/NTS.Judge.Blazor/Setup/EnduranceEvents/Participations/ParticipationFormModel.cs
+++ src/Judge/NTS.Judge.Blazor/Setup/EnduranceEvents/Participations/ParticipationFormModel.cs
@@ -1,56 +1,26 @@
 ﻿using Not.Blazor.CRUD.Forms.Ports;
-using Not.Domain.Exceptions;
 using NTS.Domain.Setup.Aggregates;
 
 namespace NTS.Judge.Blazor.Setup.EnduranceEvents.Participations;
 
 public class ParticipationFormModel : IFormModel<Participation>
 {
-    TimeSpan? _startTimeOverride;
-    double? _maxSpeedOverride;
-
     public int? Id { get; set; }
-    public bool OverrideStartTime { get; set; }
-    public TimeSpan? StartTimeOverride
-    {
-        get => _startTimeOverride;
-        set
-        {
-            OverrideStartTime = value != null;
-            _startTimeOverride = value;
-        }
-    }
+    public bool IsStartTimeOverriden { get; set; }
+    public TimeSpan? StartTimeOverride { get; set;}
     public bool IsNotRanked { get; set; }
     public Combination? Combination { get; set; }
-    public bool OverrideMaxSpeed { get; set; }
-    public double? MaxSpeedOverride
-    {
-        get => _maxSpeedOverride;
-        set
-        {
-            OverrideMaxSpeed = value != null;
-            _maxSpeedOverride = value;
-        }
-    }
+    public bool IsMaxSpeedOverriden { get; set; }
+    public double? MaxSpeedOverride { get; set; }
 
     public void FromEntity(Participation participation)
     {
         Id = participation.Id;
+        IsStartTimeOverriden = participation.StartTimeOverride != null;
         StartTimeOverride = participation.StartTimeOverride?.LocalDateTime.TimeOfDay;
         IsNotRanked = participation.IsNotRanked;
         Combination = participation.Combination;
+        IsMaxSpeedOverriden = participation.MaxSpeedOverride != null;
         MaxSpeedOverride = participation.MaxSpeedOverride;
-    }
-
-    public void ValidateConditionalInputs()
-    {
-        if (OverrideStartTime && StartTimeOverride == null)
-        {
-            throw new DomainException(Null_or_malformed_string, Start_Time_string);
-        }
-        if (OverrideMaxSpeed && MaxSpeedOverride == null)
-        {
-            throw new DomainException(Null_or_malformed_string, Max_Speed_Penalty_string);
-        }
     }
 }
--- src/Judge/NTS.Judge/Features/Setup/Participations/SetupParticipationBehind.cs
+++ src/Judge/NTS.Judge/Features/Setup/Participations/SetupParticipationBehind.cs
@@ -1,5 +1,6 @@
 ﻿using Not.Application.Behinds.Adapters;
 using Not.Application.CRUD.Ports;
+using Not.Domain.Exceptions;
 using Not.Extensions;
 using NTS.Domain.Setup.Aggregates;
 using NTS.Judge.Blazor.Setup.EnduranceEvents.Participations;
@@ -17,16 +18,28 @@ public class SetupParticipationBehind : CrudChildBehind<Participation, Participa
 
     protected override Participation CreateEntity(ParticipationFormModel model)
     {
-        model.ValidateConditionalInputs();
-        var newStart = model.StartTimeOverride?.ToDateTimeOffset();
+        ValidateEntity(model);
+        var newStart = model.IsStartTimeOverriden ? model.StartTimeOverride?.ToDateTimeOffset() : null;
         return Participation.Create(newStart, model.IsNotRanked, model.Combination, model.MaxSpeedOverride);
     }
 
     protected override Participation UpdateEntity(ParticipationFormModel model)
     {
-        model.ValidateConditionalInputs();
-        var newStart = model.StartTimeOverride?.ToDateTimeOffset();
+        ValidateEntity(model);
+        var newStart = model.IsStartTimeOverriden ? model.StartTimeOverride?.ToDateTimeOffset() : null;
         return Participation.Update(model.Id, newStart, model.IsNotRanked, model.Combination, model.MaxSpeedOverride);
+    }
+
+    public void ValidateEntity(ParticipationFormModel model)
+    {
+        if (model.IsStartTimeOverriden && model.StartTimeOverride == null)
+        {
+            throw new DomainPropertyException(nameof(model.StartTimeOverride), Null_or_malformed_string, Start_Time_string);
+        }
+        if (model.IsMaxSpeedOverriden && model.MaxSpeedOverride == null)
+        {
+            throw new DomainPropertyException(nameof(model.MaxSpeedOverride), Null_or_malformed_string, Max_Speed_Penalty_string);
+        }
     }
 
     // public async Task Reflect(Combination update)
