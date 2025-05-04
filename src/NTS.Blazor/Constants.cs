743333935d2f986c35d1a64c282c08b8d690d35a
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2025-05-04 11:27:42 +0300

    add NTS.Blazor project for common Components used across platforms

---
+++ src/NTS.Blazor/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace NTS.Blazor
+{
+    public static class Constants
+    {
+        public const string MINUTES_TIME_MASK_FORMAT = "00:00";
+        public const string SECONDS_TIME_MASK_FORMAT = "00:00:00";
+    }
+}
