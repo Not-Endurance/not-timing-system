8dcd90e683454f8210c696c6f848cc22865e2999
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2025-05-08 15:45:22 +0300

    use ParticipationTable at Witness Perfomance page

---
+++ src/Witness/NTS.Witness/Components/Pages/Performance.razor.cs
@@ -0,0 +1,21 @@
+﻿using NTS.Domain.Core.Aggregates.Participations;
+using NTS.Domain.Objects;
+
+namespace NTS.Witness.Components.Pages;
+
+public partial class Performance
+{
+    List<NTS.Domain.Core.Aggregates.Participations.Phase> _phases = [];
+    PhaseCollection? _phasesCollection;
+    Person _participant;
+
+    protected override void OnInitialized()
+    {
+        _participant = new Person(["Todomir", "Stroinov"]);
+        var phase1 = new NTS.Domain.Core.Aggregates.Participations.Phase(20, 15, 40, NTS.Domain.Enums.CompetitionRuleset.Regional, false, null, DateTimeOffset.Now);
+        var phase2 = new NTS.Domain.Core.Aggregates.Participations.Phase(20, 20, 40, NTS.Domain.Enums.CompetitionRuleset.Regional, true, null, null);
+        _phases.Add(phase1);
+        _phases.Add(phase2);
+        _phasesCollection = new(_phases);
+    }
+}
