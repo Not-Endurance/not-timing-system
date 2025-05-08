2d30f7d41fee1fcc3402386bffd117625a0c29e1
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2025-05-08 15:46:35 +0300

    use Startlist at Witness Startlist page

---
+++ src/Witness/NTS.Witness/Components/Pages/Startlist.razor.cs
@@ -0,0 +1,18 @@
+﻿using NTS.Domain.Core.Objects.Startlists;
+using NTS.Domain.Objects;
+
+namespace NTS.Witness.Components.Pages;
+
+public partial class Startlist
+{
+    List<StartlistEntry> _startlistEntries = [];
+
+    protected override void OnInitialized()
+    {
+        var startlistEntry1 = new StartlistEntry(new Person(["Todomir", "Stroinov"]), 24, 2, 20, DateTimeOffset.Now.AddMinutes(12));
+        var startlistEntry2 = new StartlistEntry(new Person(["Antonia", "Kraiselska"]), 36, 2, 20, DateTimeOffset.Now.AddMinutes(9));
+        _startlistEntries.Add(startlistEntry1);
+        _startlistEntries.Add(startlistEntry2);
+    }
+}
+
