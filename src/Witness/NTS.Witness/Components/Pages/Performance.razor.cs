3b9a06072addbce974c7ce8632da991d40c44682
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2025-05-16 20:55:28 +0300

    fix Performance demo

--- src/Witness/NTS.Witness/Components/Pages/Performance.razor.cs
+++ src/Witness/NTS.Witness/Components/Pages/Performance.razor.cs
@@ -7,12 +7,12 @@ public partial class Performance
 {
     List<NTS.Domain.Core.Aggregates.Participations.Phase> _phases = [];
     PhaseCollection? _phasesCollection;
-    Person _participant;
+    Person? _participant;
 
     protected override void OnInitialized()
     {
         _participant = new Person(["Todomir", "Stroinov"]);
-        var phase1 = new NTS.Domain.Core.Aggregates.Participations.Phase(20, 15, 40, NTS.Domain.Enums.CompetitionRuleset.Regional, false, null, DateTimeOffset.Now);
+        var phase1 = new NTS.Domain.Core.Aggregates.Participations.Phase(20, 15, 40, NTS.Domain.Enums.CompetitionRuleset.Regional, false, null, DateTimeOffset.Now.AddMinutes(23));
         var phase2 = new NTS.Domain.Core.Aggregates.Participations.Phase(20, 20, 40, NTS.Domain.Enums.CompetitionRuleset.Regional, true, null, null);
         _phases.Add(phase1);
         _phases.Add(phase2);
