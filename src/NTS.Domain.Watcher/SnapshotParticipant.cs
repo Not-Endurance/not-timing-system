58f7cf93b7ed24a390d849f37648fdeed697d9dd
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2025-05-31 15:09:54 +0300

    create SnapshotParticipant DomainObject at Watcher Domain

---
+++ src/NTS.Domain.Watcher/SnapshotParticipant.cs
@@ -0,0 +1,32 @@
+﻿using Not.Domain.Base;
+using NTS.Domain.Enums;
+using NTS.Domain.Objects;
+
+namespace NTS.Domain.Watcher;
+
+public record SnapshotParticipant : DomainObject
+{
+    public SnapshotParticipant(int number, Person athlete, Timestamp timestamp)
+    {
+        Number = number;
+        Athlete = athlete;
+        Timestamp = timestamp;
+    }
+
+    public SnapshotParticipant(int number, Person athlete)
+    {
+        Number = number;
+        Athlete = athlete;
+    }
+
+    public int Number { get; }
+    public Person Athlete { get; }
+    public Timestamp? Timestamp { get; set; }
+
+    public override string ToString()
+    {
+        return $"#{Number}: {Athlete}";
+    }
+}
+
+
