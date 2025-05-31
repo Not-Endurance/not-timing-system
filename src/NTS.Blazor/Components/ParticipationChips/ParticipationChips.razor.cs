59256963810dc6dd8ca31e06c2432efb48df5f2e
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2025-05-31 15:08:26 +0300

    extract ParticipationChips component in NTS.Blazor

---
+++ src/NTS.Blazor/Components/ParticipationChips/ParticipationChips.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using NTS.Domain.Core.Aggregates;
+
+namespace NTS.Blazor.Components.ParticipationChips;
+
+public partial class ParticipationChips
+{
+    [Parameter]
+    public Participation InitialBinding { get; set; } = default!;
+
+    [Parameter, EditorRequired]
+    public IEnumerable<Participation> ParticipationCollection { get; set; } = default!;
+
+    [Parameter]
+    public Size ChipSize { get; set; } = Size.Medium;
+
+    [Parameter]
+    public Func<Participation, Color>? GetColorAction { get; set; } = default!;
+
+    [Parameter]
+    public Action<Participation>? ClickAction { get; set; } = default!;
+
+    void ClickHandler(Participation participation)
+    {
+        ClickAction?.Invoke(participation);
+    }
+}
