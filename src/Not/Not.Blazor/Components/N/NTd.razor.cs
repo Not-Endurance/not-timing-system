f4fbba327bd17aad4b87a331ba46e580484f429a
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2025-08-23 09:54:25 +0300

    highlight CRI and RI Participation table cells

--- src/Not/Not.Blazor/Components/N/NTd.razor.cs
+++ src/Not/Not.Blazor/Components/N/NTd.razor.cs
@@ -4,4 +4,8 @@ public partial class NTd<T>
 {
     [Parameter]
     public T? Value { get; set; }
+    [Parameter]
+    public string? TextStyle { get; set; }
+    [Parameter] 
+    public string? CellStyle { get; set; }
 }
