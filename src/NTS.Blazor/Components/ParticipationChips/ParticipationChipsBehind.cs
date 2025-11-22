52eb9d59bb319098edc290d31208be62e1e85682
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2025-11-22 21:47:56 +0200

    fix ParticipationChips inheritance bug

--- src/NTS.Blazor/Components/ParticipationChips/ParticipationChipsBehind.cs
+++ src/NTS.Blazor/Components/ParticipationChips/ParticipationChipsBehind.cs
@@ -3,4 +3,15 @@ using NTS.Domain.Core.Aggregates;
 
 namespace NTS.Blazor.Components.ParticipationChips;
 
-public class ParticipationChipsBehind : NChipsBase<Participation> { }
+public class ParticipationChipsBehind : NChipsBase<Participation> 
+{
+    protected Action<Participation> _selectAction = default!;
+
+    protected override void OnParametersSet()
+    {
+        if(SelectAction is not null)
+        {
+            _selectAction = SelectAction;
+        }
+    }
+}
