74601e0f7f780e0925f152cc70cdade162cd062a
achobanov <alexchobanov@gmail.com>
2026-01-04 17:19:45 +0200

    Remove unnecessray stuff from repo

--- scripts/vup-dlls-untrack.sh
+++
@@ -1,4 +0,0 @@
-# Run from project root
-git update-index --assume-unchanged src-v2/Judge/NTS.Judge/rfid.dll
-git update-index --assume-unchanged src-v2/Judge/NTS.Judge/Vup.dll
-git update-index --assume-unchanged src-v2/Judge/NTS.Judge/rfid_core.dll
