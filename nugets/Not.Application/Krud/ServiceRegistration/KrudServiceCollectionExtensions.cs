d75417625d3115311e791a980f7f0c4392a5644d
achobanov <alexchobanov@gmail.com>
2026-01-16 01:04:45 +0200

    Prevent Transient scop

--- nugets/Not.Application/Krud/ServiceRegistration/KrudServiceCollectionExtensions.cs
+++ nugets/Not.Application/Krud/ServiceRegistration/KrudServiceCollectionExtensions.cs
@@ -29,6 +29,11 @@ public static class KrudServiceCollectionExtensions
         ServiceLifetime lifetime = ServiceLifetime.Singleton)
         where T : AggregateRoot
     {
+        if (lifetime == ServiceLifetime.Transient)
+        {
+            throw new InvalidOperationException($"Krud aggregates cannot be with Transient lifetime. Aggregate '{typeof(T).FullName}'");
+        }
+
         var meta = KrudGraphMetadata.Build(typeof(T));
         if (meta.IsFlatAggregate)
         {
