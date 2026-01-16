f9e9fb9ee4fa80ef09978d533d899735d2426f12
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2026-01-16 14:15:27 +0200

    add dummy implementation for IConnectedEventContext
    needed to avoid circular dependency in NTS.Judge by using the actual
    implementation in NTS.Storage; dummy can be removed, if WarpContext is
    abstracted from NTS.Judge

---
+++ src/Apps/Warp/NTS.Warp/Dummy/SelectedEventStore.cs
@@ -0,0 +1,17 @@
+﻿using NTS.Application.Warp;
+using NTS.Domain.Setup.Aggregates;
+
+namespace NTS.Warp.Dummy;
+
+public class SelectedEventStore : IConnectedEventContext
+{
+    public Task<UpcomingEvent?> Initialize()
+    {
+         return Task.FromResult<UpcomingEvent?>(null);
+    }
+
+    public Task Set(UpcomingEvent upcomingEvent)
+    {
+        return Task.CompletedTask;
+    }
+}
--- src/NTS.Application/Warp/IConnectedEventContext.cs
+++ src/NTS.Application/Warp/IConnectedEventContext.cs
@@ -1,8 +1,9 @@
-﻿using NTS.Domain.Setup.Aggregates;
+﻿using Not.Injection;
+using NTS.Domain.Setup.Aggregates;
 
 namespace NTS.Application.Warp;
 
-public interface IConnectedEventContext
+public interface IConnectedEventContext : ISingleton
 {
     Task<UpcomingEvent?> Initialize();
     Task Set(UpcomingEvent upcomingEvent);
