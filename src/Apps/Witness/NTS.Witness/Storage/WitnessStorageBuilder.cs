5e82906617e5477e2d454506591b7312e18d11b0
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2026-01-18 23:02:59 +0200

    implement separate UpcomingEventRepository to avoid mixup between Judge and Witness via NTS.Storage

---
+++ src/Apps/Witness/NTS.Witness/Storage/WitnessStorageBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Not.Filesystem;
+using Not.Storage;
+
+namespace NTS.Witness.Storage;
+public static class WitnessStorageServices
+{
+    // Necessary to be called directly from UI project, otherwise the runtime treeshakes this
+    // DLL off, because no resources are explicitly referenced.
+    public static Builder ConfigureWitnessStorage(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        string debugRootDirectoryName = "nts"
+    )
+    {
+        FileContextHelper.SetDebugRootDirectory(debugRootDirectoryName);
+        return new(services, configuration);
+    }
+
+    public class Builder
+    {
+        readonly NStorageBuilder _nStorageBuilder;
+        readonly IServiceCollection _services;
+
+        internal Builder(IServiceCollection services, IConfiguration configuration)
+        {
+            _nStorageBuilder = new(services, configuration);
+            _services = services;
+        }
+
+        public Builder AddMongoStorage(string? connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ApplicationException("MongoDB connection string is null");
+            }
+            _nStorageBuilder.AddMongoStorage(connectionString);
+            return this;
+        }
+
+        public Builder AddRestApiStorage()
+        {
+            _nStorageBuilder.AddRestApiStorage(Assembly.GetExecutingAssembly());
+            return this;
+        }
+    }
+}
