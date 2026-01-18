3cb28f3435a2a10a02f4bf98964d1639fe633cdc
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2026-01-18 23:10:41 +0200

    test connection

---
+++ src/Apps/Witness/NTS.Witness.Blazor/Components/EnduranceEvents/EnduranceEventsBehind.cs
@@ -0,0 +1,26 @@
+﻿using Not.Application.RPC;
+using Not.Blazor.Components;
+using NTS.Domain.Setup.Aggregates;
+using NTS.Witness.Services;
+
+public class EnduranceEventsBehind : ComponentBase
+{
+    protected IEnumerable<UpcomingEvent> Events = [];
+
+    [Inject]
+    IWitnessEvents WitnessEvents { get; set; } = default!;
+
+    [Inject]
+    IRpcContext<UpcomingEvent> RpcContext { get; set; } = default!;
+
+    [Inject]
+    IRpcInitializer RpcInitializer { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        Events = await WitnessEvents.Get();
+        await RpcContext.Set(Events.First());
+        await RpcInitializer.StartConnection();
+    }
+
+}
