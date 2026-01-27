9f05100f4fa5d3e95ac71369c0aa24b1feec4c60
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2026-01-27 16:10:42 +0200

    resolve compilation error in NTS.Judge.Tests

--- tests/NTS.Judge.Tests/WitnessTestClient.cs
+++ tests/NTS.Judge.Tests/WitnessTestClient.cs
@@ -1,26 +1,26 @@
 ﻿using Not.Application.RPC.Clients;
 using Not.Application.RPC.SignalR;
+using Not.Collections;
 using Not.Tests.RPC;
-using NTS.Warp.ACL.Entities;
-using NTS.Warp.ACL.Enums;
-using NTS.Warp.ACL.RPC.Procedures;
+using NTS.Domain.Core.Aggregates;
+using NTS.Domain.Core.Objects.Startlists;
+using NTS.Warp.Features.Witness.Procedures;
 using Xunit.Abstractions;
 
 namespace NTS.Judge.Tests;
 
 public class WitnessTestClient
     : RpcClient,
-        IEmsParticipantsClientProcedures,
-        IEmsStartlistClientProcedures,
+        IWitnessClientProcedures,
         ITestRpcClient
 {
     public WitnessTestClient(IRpcSocket socket, ITestOutputHelper _)
         : base(socket)
     {
-        RegisterInputProcedure<EmsStartlistEntry, EmsCollectionAction>(nameof(ReceiveEntry), ReceiveEntry);
-        RegisterInputProcedure<EmsParticipantEntry, EmsCollectionAction>(
-            nameof(ReceiveEntryUpdate),
-            ReceiveEntryUpdate
+        RegisterInputProcedure<StartlistEntry, NCollectionAction>(nameof(ReceiveStartlistEntry), ReceiveStartlistEntry);
+        RegisterInputProcedure<Participation, NCollectionAction>(
+            nameof(ReceiveParticipation),
+            ReceiveParticipation
         );
     }
 
@@ -33,15 +33,15 @@ public class WitnessTestClient
         InvokedMethods.Clear();
     }
 
-    public Task ReceiveEntry(EmsStartlistEntry entry, EmsCollectionAction action)
+    public Task ReceiveStartlistEntry(StartlistEntry entry, NCollectionAction action)
     {
-        InvokedMethods.Add(nameof(ReceiveEntry));
+        InvokedMethods.Add(nameof(ReceiveStartlistEntry));
         return Task.CompletedTask;
     }
 
-    public Task ReceiveEntryUpdate(EmsParticipantEntry entry, EmsCollectionAction action)
+    public Task ReceiveParticipation(Participation entry, NCollectionAction action)
     {
-        InvokedMethods.Add(nameof(ReceiveEntryUpdate));
+        InvokedMethods.Add(nameof(ReceiveParticipation));
         return Task.CompletedTask;
     }
 
