5446d2bd6f9cda6873f299c0c77d36919013d00a
achobanov <alexchobanov@gmail.com>
2026-02-06 20:40:47 +0200

    Create NNumericField

--- src/Apps/Judge/NTS.Judge/Features/Setup/UpcomingEvents/Combinations/CombinationBehind.cs
+++ src/Apps/Judge/NTS.Judge/Features/Setup/UpcomingEvents/Combinations/CombinationBehind.cs
@@ -1,11 +1,12 @@
 ﻿using Not.Application.CRUD.Ports;
+using Not.Injection;
 using Not.Krud.Abstractions;
 using Not.Krud.Services;
 using NTS.Domain.Setup.Aggregates.UpcomingEvents;
 
 namespace NTS.Judge.Features.Setup.UpcomingEvents.Combinations;
 
-public class CombinationBehind : KrudServiceBase<Combination, CombinationFormModel>
+public class CombinationBehind : KrudServiceBase<Combination, CombinationFormModel>, ITransient
 {
     public CombinationBehind(IRepository<Combination> combinations, IEnumerable<IKrudMirror<Combination>> reflections)
         : base(combinations, reflections) { }
