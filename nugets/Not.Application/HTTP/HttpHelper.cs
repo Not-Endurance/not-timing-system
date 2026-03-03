99d2498d44becc644cc47803d786c08981e125e4
achobanov <alexchobanov@gmail.com>
2026-03-03 17:36:16 +0200

    Fix HttpHelper implementation

--- nugets/Not.Application/HTTP/HttpHelper.cs
+++ nugets/Not.Application/HTTP/HttpHelper.cs
@@ -10,11 +10,11 @@ public static class HttpHelper
         }
         if (uri.StartsWith('/'))
         {
-            return (string)uri.Skip(1);
+            return string.Join("", uri.Skip(1));
         }
         if (uri.EndsWith('/'))
         {
-            return (string)uri.SkipLast(1);
+            return string.Join("", uri.SkipLast(1));
         }
         return uri;
     }
