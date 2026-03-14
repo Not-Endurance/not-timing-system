23b8a0e0e8d27ef575665b2381850a6b68c3cd93
achobanov <alexchobanov@gmail.com>
2026-03-14 11:57:13 +0200

    Fix Performance page loading

--- nugets/Not.Blazor/Components/Input/NSelectBehind.cs
+++ nugets/Not.Blazor/Components/Input/NSelectBehind.cs
@@ -1,21 +1,50 @@
-using MudBlazor;
+using System.Linq.Expressions;
 using Not.Structures;
 
 namespace Not.Blazor.Components.Input;
 
-public class NSelectBehind<T> : MudSelect<T>
+public class NSelectBehind<T> : ComponentBase
 {
-    [Parameter]
-    public new IEnumerable<NotListModel<T>> Items { get; set; } = default!;
+    protected IEnumerable<NotListModel<T>> ResolvedItems { get; private set; } = [];
 
-    protected override void OnInitialized()
+    protected Expression<Func<T>>? InputFor => For ?? ValueExpression;
+
+    [Parameter]
+    public IEnumerable<NotListModel<T>> Items { get; set; } = [];
+
+    [Parameter]
+    public T Value { get; set; } = default!;
+
+    [Parameter]
+    public EventCallback<T> ValueChanged { get; set; }
+
+    [Parameter]
+    public Expression<Func<T>>? ValueExpression { get; set; }
+
+    [Parameter]
+    public Expression<Func<T>>? For { get; set; }
+
+    [Parameter]
+    public string? Placeholder { get; set; }
+
+    [Parameter]
+    public string? Label { get; set; }
+
+    [Parameter]
+    public string? HelperText { get; set; }
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object>? UserAttributes { get; set; }
+
+    protected override void OnParametersSet()
     {
         var type = typeof(T);
         if (type.IsEnum || (Nullable.GetUnderlyingType(type)?.IsEnum ?? false))
         {
-            Items = NotListModel.FromEnum<T>(type).ToList();
+            ResolvedItems = NotListModel.FromEnum<T>(type).ToList();
+            return;
         }
 
-        base.OnInitialized();
+        ResolvedItems = Items;
     }
 }
